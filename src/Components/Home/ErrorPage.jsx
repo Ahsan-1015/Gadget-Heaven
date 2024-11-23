8bfdef475b6d5e10cfdf35b949741094b7a2ab04
Ahsan-1015 <ahshanh964@gmail.com>
2024-11-23 23:19:02 +0600

    added 404 Error page

--- src/Components/Home/ErrorPage.jsx
+++ src/Components/Home/ErrorPage.jsx
@@ -1,11 +1,34 @@
-
-
 const ErrorPage = () => {
   return (
-    <div className="text-center font-bold text-5xl mt-32" >
-      404 NO DATA AVAILAVLE
+    <div
+      className="flex items-center justify-center h-screen bg-gray-100 p-10"
+      style={{
+        backgroundImage:
+          'url("https://www.transparenttextures.com/patterns/soft-scratches.png"), linear-gradient(to bottom, #e0f7fa, #ffffff)',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="text-center p-20 hover:shadow-xl shadow-orange-300 hover:shadow-teal-500 bg-white bg-opacity-70 rounded-lg shadow-lg">
+        <h1
+          className="text-5xl font-bold text-red-500 animate__animated animate__fadeInDown"
+          data-aos="fade-down"
+        >
+          404 - Page Not Found
+        </h1>
+        <p className="text-lg mt-4 text-gray-700" data-aos="fade-up">
+          Sorry, we couldn't find the page{' '}
+          <strong className="text-gray-900">{location.pathname}</strong>.
+        </p>
+        <a
+          href="/"
+          className="mt-6 inline-block bg-gradient-to-r from-blue-400 to-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:scale-105 transform transition-all duration-300"
+          data-aos="zoom-in"
+        >
+          Go back to Home
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
