4c83b8afba36243434714689ef403b87fb41930d
Ahsan-1015 <ahshanh964@gmail.com>
2024-11-25 20:59:22 +0600

    gadegets single card added and category.json

--- src/Components/Home/Gadgets.jsx
+++ src/Components/Home/Gadgets.jsx
@@ -1,28 +1,39 @@
-import { Link,} from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 /* eslint-disable react/prop-types */
 const Gadgets = ({ data }) => {
-
-
-
-    const { product_image, product_title, price ,id } = data
-    return (
-        <div className="card bg-base-100  shadow-md border-2">
-            <figure className=" p-5 ">
-                <img
-                    src={product_image}
-                    alt="Products"
-                    className="rounded-xl  h-52 " />
-            </figure>
-            <div className="card-body pt-0">
-                <h2 className="card-title font-bold ">{product_title}</h2>
-                <p className="font-medium" ><span>Price:</span> $ {price} </p>
-                <div className="card-actions">
-                    <Link to={`/details/${id}`}>  <button className="btn  text-prime border-2 font-medium hover:bg-prime hover:text-white border-prime rounded-full  ">View Details</button></Link>
-                </div>
-            </div>
+  const { product_image, product_title, price, id } = data;
+  return (
+    <div className="card card-compact bg-base-100  shadow-md border-2">
+      <div className="rounded-lg p-4 h-56 2xl:h-64">
+        {product_image ? (
+          <img
+            src={product_image}
+            alt={data}
+            className="object-cover border-2 rounded-lg h-full w-full"
+          />
+        ) : (
+          <div className="h-44 2xl:h-56 flex  card card-compact justify-center items-center bg-gray-100 text-gray-500">
+            No Image
+          </div>
+        )}
+      </div>
+      <div className="card-body pt-0">
+        <h2 className="card-title font-bold ">{product_title}</h2>
+        <p className="font-medium">
+          <span>Price:</span> $ {price}{' '}
+        </p>
+        <div className="card-actions">
+          <Link to={`/details/${id}`}>
+            {' '}
+            <button className="btn  text-prime border-2 font-medium hover:bg-prime hover:text-white border-prime rounded-full  ">
+              View Details
+            </button>
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Gadgets
+export default Gadgets;
